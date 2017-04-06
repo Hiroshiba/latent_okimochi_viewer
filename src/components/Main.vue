50f80833067b96c4c8b76e3f469389af4b2c9766
Hiroshiba Kazuyuki <kazuyuki_hiroshiba@dwango.co.jp>
2017-04-07 02:05:19 +0900

    fetch draw api

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,13 +1,38 @@
 <template>
-  <div>{{ $store.state.config.latentShape }}</div>
+  <div>
+    <button @click="addImage">追加</button>
+    <template v-for="url in urls">
+      <img :src="url">
+    </template>
+  </div>
 </template>
 
 <script>
   import utlity from '@/utility'
 
   export default {
-    mounted () {
-      console.log(utlity.randomLatent(this.$store.state.config.latentShape).tolist())
+    data () {
+      return {
+        'urls': []
+      }
+    },
+
+    methods: {
+      addImage () {
+        const latent = utlity.randomLatent(this.$store.state.config.latentShape)
+
+        const body = new FormData()
+        body.append(this.$store.state.config.queryName, JSON.stringify(latent.tolist()))
+
+        fetch(this.$store.state.config.apiUrl, {
+          method: 'POST',
+          mode: 'cors',
+          body
+        })
+          .then(response => response.blob())
+          .then(blob => URL.createObjectURL(blob))
+          .then(url => this.urls.push(url))
+      }
     }
   }
 </script>
--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export default class {
   constructor (json) {
     this.apiUrl = json['api_url']
+    this.queryName = json['query_name']
     this.latentShape = json['latent_shape']
   }
 }
